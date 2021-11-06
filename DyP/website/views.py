836597a1ebd0db4fa998d4faf3a2fc29461f74da
lyam77 <kronos2k17@gmail.com>
2021-11-06 10:57:18 +0100

    commit on Master

---
+++ DyP/website/views.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(date=note, user_id=current_user)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+    
+
+
